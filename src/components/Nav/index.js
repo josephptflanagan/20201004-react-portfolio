53d0660edfb65ed56fff1c8b9bcff6778d3a6ff4
Joe Flanagan <josephptflanagan@gmail.com>
2020-10-16 07:01:46 -0500

    fixed the home link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,7 @@ function Nav(props) {
     const {
         categories = [],
         setCurrentCategory,
-        currentPage, 
+        currentPage,
         handlePageChange
     } = props;
 
@@ -13,41 +13,39 @@ function Nav(props) {
         document.title = capitalizeFirstLetter(currentPage);
     }, [currentPage]);
 
-    function applicationClickHandler(category){
+    function applicationClickHandler(category) {
         handlePageChange(capitalizeFirstLetter(category.name));
         setCurrentCategory(category);
     }
 
     return (
         <header className="flex-row px-1">
-            <h2>
-                <a data-testid="link" href="/">
-                    Joseph Flanagan
-        </a>
-            </h2>
             <nav>
                 <ul className="flex-row">
+                    <li className={`mx-2`}>
+                        <h2 onClick={() => handlePageChange("About")}>Joseph Flanagan</h2>
+                    </li>
                     <li className={`mx-2 ${currentPage === "About" && `navActive`}`}>
-                        <span onClick={()=>handlePageChange("About")} className={currentPage === "About" ? 'nav-link active' : 'nav-link'}>About me</span>
+                        <span onClick={() => handlePageChange("About")} className={currentPage === "About" ? 'nav-link active' : 'nav-link'}>About me</span>
                     </li>
                     <li className={`mx-2 ${currentPage === "Contact" && `navActive`}`}>
-                        <span onClick={()=>handlePageChange("Contact")} className={currentPage === "Contact" ? 'nav-link active' : 'nav-link'}>Contact</span>
+                        <span onClick={() => handlePageChange("Contact")} className={currentPage === "Contact" ? 'nav-link active' : 'nav-link'}>Contact</span>
                     </li>
                     {categories.map((category) => (
                         <li
-                            className={`mx-1 ${ currentPage === capitalizeFirstLetter(category.name) && `navActive`}`}
+                            className={`mx-1 ${currentPage === capitalizeFirstLetter(category.name) && `navActive`}`}
                             key={category.name}
                         >
-                            <span 
-                                onClick={()=> applicationClickHandler(category)}
+                            <span
+                                onClick={() => applicationClickHandler(category)}
                                 className={currentPage === capitalizeFirstLetter(category.name) ? 'nav-link active' : 'nav-link'}
-                                > 
+                            >
                                 {capitalizeFirstLetter(category.name)}
-                                </span>
+                            </span>
                         </li>
                     ))}
                     <li className={`mx-2 ${currentPage === "Resume" && `navActive`}`}>
-                    <span onClick={()=>handlePageChange("Resume")} className={currentPage === "Resume" ? 'nav-link active' : 'nav-link'}>Resume</span>
+                        <span onClick={() => handlePageChange("Resume")} className={currentPage === "Resume" ? 'nav-link active' : 'nav-link'}>Resume</span>
                     </li>
                 </ul>
             </nav>

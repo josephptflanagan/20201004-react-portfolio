68810951956445b5e82afd27ace69bab3eb5b7b1
Joe Flanagan <josephptflanagan@gmail.com>
2020-12-01 10:24:03 -0600

    added logos

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
-//import coverImage from "../../assets/cover/cover-image.jpg";
-import profilePicture from "../../assets/img/profile/profilePicture.jpg"
+import profilePicture from "../../assets/img/profile/profilePicture.jpg";
+import htmlLogo from "../../assets/icons/HTML5_Logo.svg";
+import cssLogo from "../../assets/icons/CSS3_Logo.svg";
+import jsLogo from "../../assets/icons/JavaScript_Logo.svg";
+import mongoLogo from "../../assets/icons/MongoDB_Logo.svg";
+import expressLogo from "../../assets/icons/expressjs_Logo.svg";
+import reactLogo from "../../assets/icons/React-icon.svg";
+import nodeLogo from "../../assets/icons/Node.js_logo.svg";
+import s3Logo from "../../assets/icons/aws-S3.svg";
+import cloudfrontLogo from "../../assets/icons/aws-cloudfront.svg";
+import cognitoLogo from "../../assets/icons/aws-cognito.svg";
 
 function About() {
     return (
@@ -28,9 +37,18 @@ function About() {
                     CSS, JavaScript, AJAX, jQuery, MySQL, MongoDB, Express.js, React, Node.js, HandleBars.js, Object Oriented Programing (OOP), Test Driven Development(TDD),
                     Object-Relational Mapping(ORM), Model-View-Controller(MVC), RegEx, Progressive Web Applications (PWA), and Amazon Web Services (specifically S3, Cognito, IAM, and Cloudfront).
                 </p>
-                <p>
-                    
-                </p>
+                <div>
+                    <img src={htmlLogo} alt="html5" height="64" className="logo"/>
+                    <img src={cssLogo} alt="css3" height="64" className="logo"/>
+                    <img src={jsLogo} alt="js" height="64" className="logo"/>
+                    <img src={mongoLogo} alt="mongoDB" height="64" className="logo"/>
+                    <img src={expressLogo} alt="express.js" height="64" className="logo"/>
+                    <img src={reactLogo} alt="react" height="64" className="logo"/>
+                    <img src={nodeLogo} alt="node.js" height="64" className="logo"/>
+                    <img src={s3Logo} alt="aws s3" height="64" className="logo"/>
+                    <img src={cloudfrontLogo} alt="aws cloudfront" height="64" className="logo"/>
+                    <img src={cognitoLogo} alt="aws cognito" height="64" className="logo"/>
+                </div>
             </div>
         </section>
     );
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,7 +4,7 @@ function Resume() {
 
     return (
         <section id="resume">
-            <a  href="https://drive.google.com/uc?export=download&id=1lrwQxlFgh1mKX43zzDS1EehTJB1dyWcn" download>
+            <a  href="https://drive.google.com/uc?export=download&id=1L9Ks1UFkTVJv3h_fgkZvibyhJsCzzX1O" download>
                 Click here to download a copy of my resume.
             </a>
         </section>
